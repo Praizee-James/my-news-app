1d731e178515f20e1a9d60920b2d407f304bae47
Opeyemi James <opeyemijames857@gmail.com>
2025-11-25 19:50:20 +0100

    top headline content with api integration

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -1,16 +1,22 @@
-import { useEffect, type FC } from "react"
+import { useEffect, useState, type FC } from "react"
 import { Box } from "@mui/material";
 import HeaderSection from "./HeaderSection";
 import CarouselCard from "./CarouselCard";
 import CarouselList from "./CarouselList";
 import axios from "axios";
+import type { NewsType } from "../utils/Types";
 
 
 const Carousel: FC = () => {
+    const [topHeadlines, setTopHeadlines] = useState<NewsType []> ([])
     const fetchTopHeadlines = async() =>{
     try {
         const response = await axios.get('https://newsapi.org/v2/top-headlines?country=us&apiKey=7ee071e6abf1451293a273deebbac01b')
         console.log(response.data.articles)
+        const filterHeadlines = response.data.articles.filter((
+            res:NewsType
+        ) => res.urlToImage != null)
+        setTopHeadlines(response.data.articles)
     } catch (error) {
         console.log(error)
     }
@@ -24,9 +30,9 @@ useEffect(() => {
         <Box>
             {/* Header */}
             <HeaderSection title="Top Headlines" />
-            <CarouselCard />
+            <CarouselCard topHeadline  ={topHeadlines[0]} />
             {/* Carousel list */}
-           <CarouselList/>
+           <CarouselList topHeadLines  ={topHeadlines} />
         </Box>
     )
 }
--- src/components/CarouselCard.tsx
+++ src/components/CarouselCard.tsx
@@ -1,30 +1,40 @@
-import { Box,Typography, Card, CardContent  } from '@mui/material'
+import { Box,Typography, Card, CardContent, CardMedia  } from '@mui/material'
 import {type FC} from 'react'
+import type { NewsType } from '../utils/Types'
 
-const CarouselCard:FC = () => {
+interface CarouselcardType{
+    topHeadline : NewsType
+}
+const CarouselCard:FC <CarouselcardType> = ({topHeadline}) => {
+    if (!topHeadline) return null;
+
   return (
     <Card className='grid grid-cols-2 border-2 shadow-none'>
                 <Box className=' relative h-[360px]'>
-                    <Box className='bg-red-300 h-full'></Box>
+                    
+                   < CardMedia
+                   component='img' className='h-full' image = {topHeadline?.urlToImage}
+                   />
                     <Box className='_carouselGradient' sx={{ position: 'absolute', bottom: 0, height: '70px', width: '100%' }} />
                     <Typography sx={{ fontFamily: 'serif' }} className='absolute bottom-1 text-white text-[22px] leading-8 line-clamp-3 px-6  '>
-                        Lorem Ipsum is simply dummy text of the printing and typesetting industry.
+                       {topHeadline?.title}
+                       
                     </Typography>
                 </Box>
                 {/* Card content */}
                 <CardContent className='relative'>
-                    <Typography className='text-xl font-serif line-clamp-4'>
-                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    <Typography gutterBottom className='text-xl font-serif line-clamp-4'>
+                        {topHeadline?.description}
                     </Typography>
                     <Typography className='text-sm font-serif line-clamp-4'>
-                        It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                        {topHeadline?.content}
                     </Typography>
                     <Box className='absolute bottom-2'>
                         <Typography className='text-xl font-serif line-clamp-4'>
-                            Source:CBC News
+                            Source:{topHeadline?.source.name}
                         </Typography>
                         <Typography className='text-xl font-serif line-clamp-4'>
-                            date: 11/11/2025
+                            Date: {new Date(topHeadline?.publishedAt).toLocaleDateString()}
                         </Typography>
                     </Box>
                 </CardContent>
